ceac54e0d6a0671d0e791c7793d8d556f1f4aedc
issjunior <issjunior@live.com>
2024-10-28 18:41:40 -0300

    comecando de novo

--- testes/api-yfinance.py
+++
@@ -1,8 +0,0 @@
-import yfinance as yf
-
-ativo = yf.Ticker('TRXF11.SA')
-#print(f"{ativo.dividends}") #traz todos os dividendos
-#print(f"{ativo.info}") #traz informações
-print(f"{ativo.info['longName']}")
-#print(f"{ativo.info['address1']}") #traz informação especifica
-#print(f"{ativo.info['dividendRate']}") #DY por ano
