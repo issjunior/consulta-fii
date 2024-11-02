cfd56d734fae7219f1a4921cbb3bb39a0d14232e
issjunior <issjunior@live.com>
2024-11-02 13:38:54 -0300

    abre terminal independente

--- iniciar.py
+++ iniciar.py
@@ -1,8 +1,22 @@
 import subprocess
+import sys
+import os
 
-# Caminho completo para o script Python
-script_path = "preco_teto/main.py"
+# Caminho completo para o script Python que você quer executar
+script_path = os.path.abspath("preco_teto/main.py")
 
-# Comando para abrir um novo terminal, executar o script, e manter o terminal aberto
-subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', f'python3 {script_path}; exec bash'])
+def abrir_terminal():
+    if sys.platform.startswith('linux'):
+        # Para Linux, usando gnome-terminal como exemplo
+        subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', f'python3 {script_path}; exec bash'])
+    elif sys.platform.startswith('win'):
+        # Para Windows, usando cmd
+        subprocess.Popen(['start', 'cmd', '/k', f'python {script_path}'], shell=True)
+    elif sys.platform == 'darwin':
+        # Para macOS, usando Terminal com AppleScript
+        subprocess.Popen(['osascript', '-e', f'tell app "Terminal" to do script "python3 {script_path}"'])
+    else:
+        print("Sistema operacional não suportado.")
 
+# Executa a função para abrir o terminal
+abrir_terminal()
