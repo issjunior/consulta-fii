daddadc9002c7a3470c6eee7e987ea6beaeb87f9
issjunior <issjunior@live.com>
2024-11-11 20:35:26 -0300

    organização

--- preco_teto/main.py
+++ preco_teto/main.py
@@ -33,21 +33,22 @@ with st.sidebar:
         unsafe_allow_html=True
     )
 
+    st.markdown("### IPCA")
+    st.markdown(f"<p class='reduced-space'>Busca automática no site do <a href='{url_ibge}' target='_blank'>IBGE</a></p>", unsafe_allow_html=True)
+    st.markdown(f"**IPCA**: {ipca_elements[1].text.strip()}")
+
     # Exibe os títulos encontrados e suas porcentagens
     media_ntnb_local, titulos_info = exibir_resultados()
     if titulos_info:
         st.markdown("### Títulos IPCA+ encontrados")
         st.markdown(f"<p class='reduced-space'>Busca automática no site do <a href='{url_investidor10}' target='_blank'>Investidor10</a></p>", unsafe_allow_html=True)
         for titulo, porcentagem in titulos_info:
-            st.markdown(f"<p class='reduced-space'>{titulo} - IPCA + {porcentagem}%</p>", unsafe_allow_html=True)
+            st.markdown(f"<p class='reduced-space'>{titulo} + {porcentagem}%</p>", unsafe_allow_html=True)
     else:
         st.write("Nenhum título IPCA+ encontrado ou ocorreu um erro.")
 
     # Exibição do resultado da função exibir_resultados() na barra lateral (media NTNB)
     st.markdown(f"**Média NTN-B**: {media_ntnb_local:.2f}%")
-    st.markdown("### IPCA")
-    st.markdown(f"<p class='reduced-space'>Busca automática no site do <a href='{url_ibge}' target='_blank'>IBGE</a></p>", unsafe_allow_html=True)
-    st.markdown(f"**IPCA**: {ipca_elements[1].text.strip()}")
 
 def main():
     st.header("Cálculo de preço teto para FIIs")
