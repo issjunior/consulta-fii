0f8ced3972b05766dd50e124e4bae3b425c19cb7
issjunior <issjunior@live.com>
2024-11-12 15:50:00 -0300

    ajuste de cores

--- preco_teto/main.py
+++ preco_teto/main.py
@@ -34,14 +34,14 @@ with st.sidebar:
         unsafe_allow_html=True
     )
 
-    st.markdown("### IPCA")
+    st.markdown("<h3 style='color:red;'>IPCA</h3>", unsafe_allow_html=True)
     st.markdown(f"<p class='reduced-space'>Busca automática no site do <a href='{url_ibge}' target='_blank'>IBGE</a></p>", unsafe_allow_html=True)
     st.markdown(f"**IPCA**: {ipca_elements[1].text.strip()}")
 
     # Exibe os títulos encontrados e suas porcentagens
     media_ntnb_local, titulos_info = exibir_resultados()
     if titulos_info:
-        st.markdown("### Títulos IPCA+ encontrados")
+        st.markdown("<h3 style='color:red;'>Títulos IPCA+ encontrados", unsafe_allow_html=True)
         st.markdown(f"<p class='reduced-space'>Busca automática no site do <a href='{url_investidor10}' target='_blank'>Investidor10</a></p>", unsafe_allow_html=True)
         for titulo, porcentagem in titulos_info:
             st.markdown(f"<p class='reduced-space'>{titulo} + {porcentagem}%</p>", unsafe_allow_html=True)
