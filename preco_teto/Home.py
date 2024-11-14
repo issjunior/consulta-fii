fe5fc2945130eb0886addc3eeb3cf654d25ae419
issjunior <issjunior@live.com>
2024-11-14 00:00:14 -0300

    com apresentação

---
+++ preco_teto/Home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# Caminho para o arquivo da imagem
+imagem_path = "preco_teto/img/logo_real_state_JR.png"
+
+# Exibir a imagem com o novo parâmetro
+st.image(imagem_path, caption="Sistema de Investimento", use_container_width=True)
