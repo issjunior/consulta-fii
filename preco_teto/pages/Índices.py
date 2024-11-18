00980b1f79bd0d2303708822380c519d2c343814
issjunior <issjunior@live.com>
2024-11-18 13:54:04 -0300

    dataframe

--- preco_teto/pages/Índices.py
+++ preco_teto/pages/Índices.py
@@ -82,19 +82,29 @@ st.divider()
 col1, col2 = st.columns(2)
 
 with col1:
-
     # Exibe os títulos encontrados e suas porcentagens
     media_ntnb_local, titulos_info = exibir_resultados()
+    
     if titulos_info:
         st.title("Títulos IPCA+ encontrados")
         st.caption(f"<p class='reduced-space'>Busca automática no site do <a href='{url_investidor10}' target='_blank'>Investidor10</a></p>", unsafe_allow_html=True)
-        for titulo, porcentagem in titulos_info:
-            st.markdown(f"<p class='reduced-space'>{titulo} + {porcentagem}%</p>", unsafe_allow_html=True)
+        
+        # Converte os resultados para um DataFrame
+        df_titulos = pd.DataFrame(titulos_info, columns=['Título', 'Porcentagem'])
+        
+        # Substitui a vírgula por ponto e converte a coluna 'Porcentagem' para float
+        df_titulos['Porcentagem'] = df_titulos['Porcentagem'].str.replace(',', '.').astype(float)
+        
+        # Calcula a média da coluna 'Porcentagem'
+        media_porcentagem = df_titulos['Porcentagem'].mean()
+
+        # Adiciona a média como última linha do DataFrame
+        df_titulos.loc[len(df_titulos)] = ['Média de títulos NTN-B', f"{media_porcentagem:.2f} %"]
+        
+        # Exibe o DataFrame
+        st.dataframe(df_titulos, use_container_width=True)
     else:
         st.write("Nenhum título IPCA+ encontrado ou ocorreu um erro.")
-
-    # Exibição do resultado da função exibir_resultados() na barra lateral (media NTNB)
-    st.markdown(f"**Média NTN-B**: {media_ntnb_local:.2f}%")
 
 with col2:
     st.write("Texto")
