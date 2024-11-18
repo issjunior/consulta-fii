e51e8d1408ca12fa4deada2f95a1ccf1a617b18f
issjunior <issjunior@live.com>
2024-11-18 16:55:35 -0300

    ajuste de indice

--- preco_teto/pages/Índices.py
+++ preco_teto/pages/Índices.py
@@ -11,66 +11,102 @@ st.set_page_config(
     layout="wide",
 )
 
-# Obtém os dados do IPCA (5 anos)
-ipca_5anos = sgs.get(13522)
+# Função para obter dados do IPCA
+@st.cache_data
+def obter_ipca():
+    # Obtém os dados do IPCA (5 anos)
+    ipca_5anos = sgs.get(13522)
 
-# Define a data de corte como a última data disponível
-data_corte = pd.to_datetime("today").normalize()
+    # Define a data de corte como a última data disponível
+    data_corte = pd.to_datetime("today").normalize()
 
-# Define o período padrão para 5 anos e os últimos 12 meses
-data_inicio_5anos = data_corte - pd.DateOffset(years=5)  # 5 anos de histórico
-data_inicio_12meses = data_corte - pd.DateOffset(years=1)  # Últimos 12 meses
+    # Define o período padrão para 5 anos e os últimos 12 meses
+    data_inicio_5anos = data_corte - pd.DateOffset(years=5)  # 5 anos de histórico
+    data_inicio_12meses = data_corte - pd.DateOffset(years=1)  # Últimos 12 meses
 
-# Filtra os dados para os últimos 5 anos
-ipca_filtrado = ipca_5anos.loc[data_inicio_5anos:data_corte]
+    # Filtra os dados para os últimos 5 anos
+    ipca_filtrado = ipca_5anos.loc[data_inicio_5anos:data_corte]
 
-# Inverte a ordem do DataFrame para que as datas mais recentes fiquem em cima
-ipca_filtrado = ipca_filtrado.iloc[::-1]
+    # Inverte a ordem do DataFrame para que as datas mais recentes fiquem em cima
+    ipca_filtrado = ipca_filtrado.iloc[::-1]
 
-# Remove a hora da coluna 'date' apenas no DataFrame para formatar as datas
-ipca_filtrado.index = pd.to_datetime(ipca_filtrado.index).strftime('%d/%m/%Y')
+    # Remove a hora da coluna 'date' apenas no DataFrame para formatar as datas
+    ipca_filtrado.index = pd.to_datetime(ipca_filtrado.index).strftime('%d/%m/%Y')
 
-# Converte a coluna IPCA para float (caso necessário) e formata com o símbolo de '%'
-ipca_filtrado = ipca_filtrado.apply(pd.to_numeric, errors='coerce')  # Garante que os valores sejam numéricos
-ipca_filtrado_formatado = ipca_filtrado.map(lambda x: f"{x:.2f} %" if isinstance(x, (int, float)) else x)
+    # Converte a coluna IPCA para float (caso necessário) e formata com o símbolo de '%'
+    ipca_filtrado = ipca_filtrado.apply(pd.to_numeric, errors='coerce')  # Garante que os valores sejam numéricos
+    ipca_filtrado_formatado = ipca_filtrado.map(lambda x: f"{x:.2f} %" if isinstance(x, (int, float)) else x)
+
+    return ipca_filtrado_formatado, ipca_5anos, data_inicio_5anos, data_corte
+
+# Função para criar o gráfico
+@st.cache_data
+def criar_grafico(ipca_5anos, data_inicio_5anos, data_corte):
+    # Define o início do intervalo como 12 meses atrás
+    data_inicio_12meses = data_corte - pd.DateOffset(years=1)
+
+    # Filtra os dados para os últimos 5 anos
+    ipca_filtrado_5anos = ipca_5anos.loc[data_inicio_5anos:data_corte]
+
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(
+            x=ipca_filtrado_5anos.index,
+            y=ipca_filtrado_5anos.values.flatten(),  # Transforma em vetor simples
+            mode='markers+lines',
+            name="IPCA (5 anos)",
+        )
+    )
+    
+    # Ajustes no layout do gráfico
+    fig.update_layout(
+        title="Histórico de IPCA (Últimos 5 anos)",
+        xaxis_title="Período",
+        yaxis_title="IPCA (%)",
+        xaxis_rangeslider_visible=True,  # Controle de zoom (range slider)
+        xaxis_range=[data_inicio_12meses, data_corte],  # Mostra os últimos 12 meses inicialmente
+    )
+    
+    return fig
+
+# Função para processar os títulos
+@st.cache_data
+def processar_titulos(titulos_info):
+    # Converte os resultados para um DataFrame
+    df_titulos = pd.DataFrame(titulos_info, columns=['Título', 'Porcentagem'])
+
+    # Substitui a vírgula por ponto e converte a coluna 'Porcentagem' para float
+    df_titulos['Porcentagem'] = df_titulos['Porcentagem'].str.replace(',', '.').astype(float)
+
+    # Calcula a média da coluna 'Porcentagem'
+    media_porcentagem = df_titulos['Porcentagem'].mean()
+
+    # Adiciona a média como última linha do DataFrame
+    df_titulos.loc[len(df_titulos)] = ['Média de títulos NTN-B', media_porcentagem]
+
+    # Aplica o símbolo de porcentagem a todos os valores da coluna 'Porcentagem'
+    df_titulos['Porcentagem'] = df_titulos['Porcentagem'].map(lambda x: f"{x:.2f} %")
+
+    return df_titulos
+
+# Obtém os dados IPCA
+ipca_filtrado_formatado, ipca_5anos, data_inicio_5anos, data_corte = obter_ipca()
 
 # Layout 1 em duas colunas
 col1, col2 = st.columns(2)
 
-# Coluna 1: Gráfico
+# Coluna 1: Exibe os dados IPCA
 with col1:
     st.title("IPCA")
     st.caption("Dados retirados do Banco Central (5 anos)")
-    st.dataframe(ipca_filtrado_formatado, use_container_width=True)  # Exibe os dados com '%' e usa toda a largura da coluna
+    st.dataframe(ipca_filtrado_formatado, use_container_width=True)
 
-# Coluna 2: Exibe o DataFrame dos últimos 5 anos (invertido), com '%' ao lado dos valores
+# Coluna 2: Exibe o gráfico do IPCA
 with col2:
-    #st.title("Evolução do IPCA (12 meses)")
-    
-    # Verifica se o DataFrame não está vazio
     if not ipca_5anos.empty:
-        # Criação do gráfico
-        fig = go.Figure()
-        fig.add_trace(
-            go.Scatter(
-                x=ipca_5anos.loc[data_inicio_5anos:data_corte].index,
-                y=ipca_5anos.loc[data_inicio_5anos:data_corte].values.flatten(),  # Transforma em vetor simples
-                #mode="lines",
-                mode='markers+lines',
-                name="IPCA (5 anos)",
-            )
-        )
-        
-        # Ajustes no layout do gráfico
-        fig.update_layout(
-            title="Histórico de IPCA (Últimos 5 anos)",
-            xaxis_title="Escolha o período",
-            yaxis_title="IPCA (%)",
-            xaxis_rangeslider_visible=True,  # Controle de zoom (range slider)
-            xaxis_range=[data_inicio_12meses, data_corte],  # Mostra apenas os últimos 12 meses inicialmente
-        )
-
-        # Exibe o gráfico interativo no Streamlit
+        # Cria o gráfico interativo
+        fig = criar_grafico(ipca_5anos, data_inicio_5anos, data_corte)
+        # Exibe o gráfico
         st.plotly_chart(fig, use_container_width=True)
     else:
         st.write("Dados do IPCA não disponíveis.")
@@ -83,27 +119,15 @@ col1, col2 = st.columns(2)
 with col1:
     # Exibe os títulos encontrados e suas porcentagens
     media_ntnb_local, titulos_info = exibir_resultados()
-    
+
     if titulos_info:
         st.title("Títulos IPCA+ encontrados")
         st.caption(f"Busca automática no site do <a href='{url_investidor10}' target='_blank'>Investidor10</a></p>", unsafe_allow_html=True)
-        
-        # Converte os resultados para um DataFrame
-        df_titulos = pd.DataFrame(titulos_info, columns=['Título', 'Porcentagem'])
-        
-        # Substitui a vírgula por ponto e converte a coluna 'Porcentagem' para float
-        df_titulos['Porcentagem'] = df_titulos['Porcentagem'].str.replace(',', '.').astype(float)
-        
-        # Calcula a média da coluna 'Porcentagem'
-        media_porcentagem = df_titulos['Porcentagem'].mean()
 
-        # Adiciona a média como última linha do DataFrame
-        df_titulos.loc[len(df_titulos)] = ['Média de títulos NTN-B', media_porcentagem]
-        
-        # Aplica o símbolo de porcentagem a todos os valores da coluna 'Porcentagem'
-        df_titulos['Porcentagem'] = df_titulos['Porcentagem'].map(lambda x: f"{x:.2f} %")
-        
-        # Exibe o DataFrame
+        # Processa os títulos encontrados
+        df_titulos = processar_titulos(titulos_info)
+
+        # Exibe o DataFrame com os títulos
         st.dataframe(df_titulos, use_container_width=True)
     else:
         st.write("Nenhum título IPCA+ encontrado ou ocorreu um erro.")
