dbde66da1ea429c4abc966b068540199e32f15f3
issjunior <issjunior@live.com>
2024-11-22 16:02:37 -0300

    ajuste

--- preco_teto/pages/De_olho.py
+++ preco_teto/pages/De_olho.py
@@ -16,7 +16,8 @@ def obter_dados_bitcoin():
     
     dados_btc = yf.download("BTC-USD", start=data_inicio, end=data_corte)
     dados_btc = yf.download("BTC-USD", start=data_inicio, end=data_corte)
-    dados_btc.drop(columns=["Adj Close", "Open", "Volume"], inplace=True)  # exclui a coluna Adj Close e Open
+    dados_btc.drop(columns=["Adj Close", "Open", "Volume"], inplace=True)  # exclui a coluna 'Adj Close' e 'Open'
+    dados_btc = dados_btc.sort_index(ascending=False)
     return dados_btc, data_inicio, data_corte
 
 # Obtém os dados do Bitcoin
@@ -28,20 +29,23 @@ if not dados_btc.empty:
     col1, col2, col3, col4 = st.columns(4)
     
     # Obtém os valores e converte para float
-    ultimo_preco = float(dados_btc['Close'].iloc[-1])
-    primeiro_preco = float(dados_btc['Close'].iloc[0])
+    ultimo_preco = float(dados_btc['Close'].iloc[0])
+    penultimo_preco = float(dados_btc['Close'].iloc[1])
+    primeiro_preco = float(dados_btc['Close'].iloc[-1])
+
     preco_maximo = float(dados_btc['High'].max())
+    
     preco_minimo = float(dados_btc['Low'].min())
     
     with col1:
-        st.metric("Preço Atual", f"{dolar(ultimo_preco)}")
+        st.metric(label="Preço Atual", value=dolar(ultimo_preco), delta=dolar(penultimo_preco))
     with col2:
         variacao = ((ultimo_preco - primeiro_preco) / primeiro_preco) * 100
-        st.metric("Variação no Período", f"{porcentagem(variacao)}")
+        st.metric(label="Variação no Período", value=porcentagem(variacao))
     with col3:
-        st.metric("Máxima no Período", f"{dolar(preco_maximo)}")
+        st.metric(label="Máxima no Período", value=dolar(preco_maximo))
     with col4:
-        st.metric("Mínima no Período", f"{dolar(preco_minimo)}")
+        st.metric(label="Mínima no Período", value=dolar(preco_minimo))
 
 # Adiciona informações sobre o período dos dados
 st.caption(f"Dados do período: {data_inicio.strftime('%d/%m/%Y')} até {data_corte.strftime('%d/%m/%Y')}")
@@ -53,7 +57,7 @@ col1, col2 = st.columns(2)
 with col1:
     if not dados_btc.empty:
         st.title("Dados do Bitcoin")
-        st.dataframe(dados_btc, use_container_width=True)
+        st.dataframe(dados_btc, use_container_width=True)  # limita a visualização em 7 linhas, mas permite visualizar o restante do dt
     else:
         st.error("Dados do Bitcoin não disponíveis para exibição.")
 
