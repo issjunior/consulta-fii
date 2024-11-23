66f514b739195e8fdf8e73a5faa13e0bf57c9241
issjunior <issjunior@live.com>
2024-11-22 22:17:51 -0300

    ajuste

---
+++ pages/Calculadora.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+# Configuração do layout do Streamlit
+st.set_page_config(
+    page_title="Sis. de Investimento",
+    page_icon="📊",
+    layout="wide",
+)
+
+st.title("Em construção ...")
+
+st.write("Yield On Cost")
+
