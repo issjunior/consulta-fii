80dabdb5eb852169a4c3291c122b75af5c0b86ba
issjunior <issjunior@live.com>
2024-12-08 10:46:37 -0300

    ajuste

--- pages/Títulos_Públicos.py
+++ pages/Títulos_Públicos.py
@@ -9,7 +9,6 @@ st.set_page_config(
     layout="wide",  # ou "centered"
 )
 
-
 # Exibe os títulos encontrados no investidor10 e suas porcentagens
 media_ntnb_local, titulos_info = exibir_resultados()
 
@@ -20,8 +19,24 @@ if titulos_info:
     # Processa os títulos encontrados
     df_titulos = processar_titulos(titulos_info, ultimo_ipca_formatado)
 
-    # Exibe o DataFrame com os títulos, agora incluindo a coluna 'Soma'
-    st.dataframe(df_titulos, hide_index=True, use_container_width=True)
+    # Define uma função de estilo para destacar o maior valor da coluna 'Soma'
+    def highlight_max(data, color="gray"):  # define a cor da celula
+        """
+        Destaca o maior valor na coluna 'Soma'.
+        """
+        attr = f"background-color: {color};"
+        # Cria um DataFrame vazio para aplicar o estilo
+        is_max = data == data.max()
+        return [attr if is_max else "" for is_max in is_max]
+    
+    # Aplica o estilo
+    styled_df = df_titulos.style.apply(
+        highlight_max, 
+        subset=["Soma"]  # Aplica somente na coluna "Soma"
+    )
+    
+    # Exibe o DataFrame com o estilo
+    st.table(styled_df)
     st.caption(f"Fonte: <a href='{url_investidor10}' target='_blank'>Investidor10</a>.</p>", unsafe_allow_html=True)
 else:
     st.write("Nenhum título IPCA+ encontrado ou ocorreu um erro.")
