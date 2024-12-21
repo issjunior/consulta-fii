426feab6a062044488415d74a2293d708c8271a9
issjunior <issjunior@live.com>
2024-12-21 17:00:26 -0300

    ajuste

--- pages/Resumos_FII.py
+++ pages/Resumos_FII.py
@@ -5,9 +5,15 @@ import plotly.express as px
 from datetime import datetime, timedelta
 from config import *
 
-# Configurações do Streamlit
-st.set_page_config(page_title="Análise de FIIs", layout="wide")
-st.title("Análise de FIIs")
+# Configuração do layout
+st.set_page_config(
+    page_title="Sis. de Investimento",
+    page_icon="📊",
+    layout="wide",  # ou "centered"
+)
+
+st.title("Resumo de FIIs")
+st.caption("Considerando o período de 2 anos")
 
 # Lista de tickers para análise
 tickers = ["HGLG11.SA", "TRXF11.SA", "XPML11.SA"]
@@ -38,16 +44,40 @@ for ticker in tickers:
     data = fetch_ticker_data(ticker)
     adj_close, maior_valor, menor_valor = process_ticker_data(data)
     
+    # Obter o valor corrente do ticker (último preço de mercado)
+    ticker_info = yf.Ticker(ticker)
+    ticker_history = ticker_info.history(period="1d")
+    
+    # Verificar se 'Adj Close' está presente ou usar 'Close'
+    if 'Adj Close' in ticker_history.columns:
+        ultimo_valor_corrente = ticker_history['Adj Close'].iloc[-1]
+    elif 'Close' in ticker_history.columns:
+        ultimo_valor_corrente = ticker_history['Close'].iloc[-1]
+    else:
+        ultimo_valor_corrente = None  # Caso não haja nenhum valor disponível
+    
+    # Calcular a diferença entre o maior e menor valor
+    diferenca_valores = maior_valor - menor_valor
+    
+    # Calcular a diferença percentual
+    if menor_valor != 0:
+        diferenca_percentual = ((maior_valor - menor_valor) / menor_valor) * 100
+    else:
+        diferenca_percentual = 0  # Evitar divisão por zero, caso o menor valor seja 0
+    
     # Adicionar informações ao DataFrame
     resumo = pd.DataFrame({
         "Ticker": [ticker.replace(".SA", "")],
+        "Valor": real(ultimo_valor_corrente) if ultimo_valor_corrente is not None else "N/A",
         "Maior Valor": real(maior_valor),
-        "Menor Valor": real(menor_valor)
+        "Menor Valor": real(menor_valor),
+        "Diferença (R$)": real(diferenca_valores),
+        "Diferença Percentual (%)": porcentagem(diferenca_percentual)
     })
     dataframes.append(resumo)
 
 # Concatenar todos os DataFrames
 resultados = pd.concat(dataframes, ignore_index=True)
 
-# Exibir DataFrame no Streamlit
-st.dataframe(resultados)
+# Exibir DataFrame no Streamlit sem a coluna de índice
+st.dataframe(resultados, use_container_width=True, hide_index=True)
