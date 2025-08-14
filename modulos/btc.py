8343f38ab2d62a9a05c60661d803705351bb7c45
issjunior <issjunior@live.com>
2025-08-13 20:52:46 -0400

    ajuste_BTC

--- modulos/btc.py
+++ modulos/btc.py
@@ -5,11 +5,18 @@ def obter_dados_bitcoin():
     data_corte = pd.to_datetime("today").normalize()
     data_inicio = data_corte - pd.DateOffset(years=2)  # período de pesquisa
     
-    dados_btc = yf.download("BTC-USD", start=data_inicio, end=data_corte)
-    dados_btc.drop(columns=["Adj Close", "Open", "Volume"], inplace=True)  # exclui colunas desnecessárias
-    dados_btc = dados_btc.sort_index(ascending=False)
+    dados_btc = yf.download(
+        "BTC-USD",
+        start=data_inicio,
+        end=data_corte,
+        auto_adjust=False  # garante que venha "Adj Close"
+    )
     
-    # Ajusta o índice para o formato de data brasileiro (dd/mm/aaaa)
+    # Remove colunas apenas se existirem
+    colunas_para_remover = [col for col in ["Adj Close", "Open", "Volume"] if col in dados_btc.columns]
+    dados_btc.drop(columns=colunas_para_remover, inplace=True)
+    
+    dados_btc = dados_btc.sort_index(ascending=False)
     dados_btc.index = pd.to_datetime(dados_btc.index).strftime('%d/%m/%Y')
     
     return dados_btc, data_inicio, data_corte
