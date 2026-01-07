786a3ebd5fad735b4b6185445daf8a8ecccf414d
issjunior <issjunior@live.com>
2026-01-06 21:46:33 -0300

    ajuste

--- pages/Ações.py
+++ pages/Ações.py
@@ -6,23 +6,18 @@ st.set_page_config(
     page_title="Sis. de Investimento",
     page_icon="📊",
     layout="wide",  # ou "centered"
+#   layout="centered",  # ou "wide"
 )
 
-st.title("Cálculo de Ações")
-st.caption("Método de análise de desempenho de ações")
+st.title("Cálculo de preço teto Ações")
 
 with st.expander("Entenda o cálculo"):
-    st.title("Método ")
-    st.write("Fórmula utilizada para estimar o preço justo ou preço-teto de um ativo baseado em seus dividendos futuros. No contexto de fundos imobiliários, esse modelo assume que os dividendos crescem a uma taxa constante ao longo do tempo ou não subir.")
+    st.title("Método Graham")
+    st.write("Busca determinar um valor justo para uma ação com base no lucro por ação (LPA) e na taxa de crescimento esperada da empresa")
 
     moldura = st.container(border=True)
-    moldura.image("img/modelo_gordon.png")
-
-    st.markdown(""" 
-    ##### Explicando cada termo:
-    O método Graham calcula o preço-teto (ou valor justo) de uma ação usando a fórmula: Valor Justo = √ (22,5 x LPA x VPA), onde LPA é o Lucro por Ação e VPA é o Valor Patrimonial por Ação
-    """)
-
-    st.write("Usamos os títulos NTN-B (Tesouro IPCA+) para precificar fundos imobiliários, porque eles oferecem uma taxa de retorno praticamente livre de risco e protegida contra a inflação. Essa taxa serve como base de comparação para o retorno esperado dos FIIs, já que, por terem maior risco, os fundos imobiliários precisam oferecer uma rentabilidade superior a média NTN-B. Além disso, essa comparação ajuda os investidores a avaliar se os FIIs estão caros ou baratos.")
+    moldura.image("img/formula_graham.png")
 
 st.divider()
+
+st.header("Parâmetros de entrada")
